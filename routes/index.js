c04cac694bea7a3f52eeeb3d0cd7572f10bd37d7
Nausheen32 <nausheen.fathima1202@gmail.com>
2021-11-20 18:20:10 -0600

    Login functionality

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+var monk = require('monk');
+var db = monk('localhost:27017/vitaminstore');
+var collection = db.get('users');
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('login');
 });
+
+router.post('/login', function(req, res, next) {
+  collection.findOne({username:req.body.username, password:req.body.password},function(err,user){
+    if(err) throw err;
+    res.json(user);
+   });
+  // res.render('editmovie', { myID : req.params.id });
+});
+
 
 module.exports = router;
