a6a90c358d8acb58ee57da1d6160199223b23d38
harichandanamone <harichandanamone@gmail.com>
2021-11-20 18:21:51 -0600

    sign in commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,33 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('signin');
 });
+
+
+var monk  = require('monk');
+var db = monk('localhost:27017/vitaminstore');
+
+var collection = db.get('users');
+
+
+router.post('/user',function(req,res,next){
+  var user = {
+    username : req.body.email,
+    password:req.body.password,
+    fname:req.body.firstName,
+    lname : req.body.lastName,
+  }
+
+
+  collection.insert(user,function(err,user){
+      if(err) throw err;
+      res.json(user);
+
+  })
+
+});
+
+
 
 module.exports = router;
